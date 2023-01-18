d4dd54ae10145560ef27eba2ee0a46c4db37b836
FusionTeamQA <qatestfusion@gmail.com>
2023-01-18 14:39:28 +0300

    add setting.py

---
+++ setting.py
@@ -0,0 +1,30 @@
+token = '5975275814:AAGkgWEqk3XQX25fbXIswWstgUSWIWfpe2M'
+admin_id_ugraswim = 597010493
+# admin_hr_id =
+# admin_kate_id =
+#Общие ссылки
+website = "https://fusion-tech.pro"
+blog = 'https://fusion-tech.ru/blog'
+vacansies = 'https://fusion-tech.ru/vacancies/'
+projects = 'https://fusion-tech.ru/projects'
+
+
+#Ссылки по специализациям
+
+SALES_MANAGER = 'https://career.habr.com/vacancies/1000116918'
+# QA =
+DEV_FULL = 'https://career.habr.com/vacancies/1000116914'
+# DEV_FRONT =
+# DEV_BACK =
+DESIGN = 'https://career.habr.com/vacancies/1000116917'
+# HR =
+# OFFICE_MANAGER =
+# PROJECT_MANAGER =
+
+#ВК
+
+VK = 'https://vk.com/fusion_tech'
+VK_HR = 'https://vk.com/dasha_fusion_hr'
+VK_GROUP_CHAT = 'https://vk.com/im?sel=-158106363'
+# BEHANCE = ''
+HABR = 'https://career.habr.com/companies/fusion-tech'
